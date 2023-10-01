bd87af6ee79fb9f2c1a5118868bd77ece60a289f
Xaox01 <studiocrystalgames06@gmail.com>
2023-10-01 23:48:17 +0200

    Add circle

--- components/SobrietyCounter.js
+++ components/SobrietyCounter.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   View,
   Text,
@@ -7,6 +7,10 @@ import {
   ScrollView,
   RefreshControl,
   TextInput,
+  Alert,
+  Animated,
+  TouchableHighlight,
+  Dimensions,
 } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Notifications from 'expo-notifications';
@@ -15,7 +19,7 @@ const motivationalQuotes = [
   'szatan to pała',
   'gówno',
   'pjes',
-  'test'
+  'test',
 ];
 
 const getRandomQuote = () => {
@@ -35,6 +39,7 @@ const SobrietyCounter = () => {
 
   const [reason, setReason] = useState('');
   const [showForm, setShowForm] = useState(false);
+  const borderColorAnim = useRef(new Animated.Value(0)).current;
 
   useEffect(() => {
     const getSobrietyDate = async () => {
@@ -98,33 +103,53 @@ const SobrietyCounter = () => {
       }
       setSobrietyDate(currentDate);
       setButtonLabel('Stop');
-      } else {
+    } else {
       setShowForm(true);
-      }
-      };
-      
-      const handleFormSubmit = async () => {
-      // Tutaj można dodać kod obsługujący przesłanie powodu do bazy danych lub innego miejsca
-      try {
-        await AsyncStorage.removeItem('sobrietyDate');
-        setSobrietyDate(null);
-        setTime({ days: 0, hours: 0, minutes: 0, seconds: 0 });
-        setButtonLabel('Start');
-        setResetCounter((prev) => prev + 1);
-      } catch (error) {
-        console.error('Error while removing sobriety date:', error);
-      }
-      setShowForm(false);
-      setReason('');
-    };
+    }
+  };
 
-    async function sendNotification() {
+  const handleFormSubmit = async () => {
+    if (reason.trim() === '') {
+      Alert.alert('Wprowadź powód', 'Proszę napisz dlaczego rezygnujesz z liczenia, będzie to pomocne w walce z twoim nałogiem.', [
+        { text: 'OK' },
+      ]);
+      // Animate border color
+      Animated.sequence([
+        Animated.timing(borderColorAnim, {
+          toValue: 1,
+          duration: 300,
+          useNativeDriver: false,
+        }),
+        Animated.timing(borderColorAnim, {
+          toValue: 0,
+          duration: 300,
+          useNativeDriver: false,
+        }),
+      ]).start();
+
+      return;
+    }
+
+    // Tutaj można dodać kod obsługujący przesłanie powodu do bazy danych lub innego miejsca
+    try {
+      await AsyncStorage.removeItem('sobrietyDate');
+      setSobrietyDate(null);
+      setTime({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      setButtonLabel('Start');
+      setResetCounter((prev) => prev + 1);
+    } catch (error) {
+      console.error('Error while removing sobriety date:', error);
+    }
+    setShowForm(false);
+    setReason('');
+  };
+
+  async function sendNotification() {
     const permission = await Notifications.requestPermissionsAsync();
     if (permission.status !== 'granted') {
       console.error('Permission to send notifications not granted.');
       return;
     }
-    
     const identifier = await Notifications.scheduleNotificationAsync({
       content: {
         title: 'Gratulacje!',
@@ -134,79 +159,103 @@ const SobrietyCounter = () => {
         seconds: 10, // testowo ustawić na 10 sekund
       },
     });
-    
+
     console.log('Notification scheduled:', identifier);
   }
 
+  const borderColor = borderColorAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['#FFFFFF', '#FF0000'],
+  });
+
   return (
-  <ScrollView
-  contentContainerStyle={styles.container}
-  refreshControl={
-  <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-  }
-  >
-  <Text style={styles.timeText}>
-  {time.days} dni {time.hours} godz. {time.minutes} min. {time.seconds} sek.
-  </Text>
-  <Button title={buttonLabel} onPress={handleButtonClick} />
-  {showForm && (
-  <View style={styles.form}>
-  <TextInput
-           style={styles.reasonInput}
-           onChangeText={setReason}
-           value={reason}
-           placeholder="Wprowadź powód"
-         />
-  <Button title="Zatwierdź" onPress={handleFormSubmit} />
-  </View>
-  )}
-  <View style={styles.quoteContainer}>
-  <Text style={styles.quoteText}>{quote}</Text>
-  </View>
-  </ScrollView>
+    <ScrollView
+      contentContainerStyle={styles.container}
+      refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+    >
+      <Text style={styles.timeText}>
+        {time.days} dni {time.hours} godz. {time.minutes} min. {time.seconds} sek.
+      </Text>
+      <TouchableHighlight
+        style={{
+          borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+          width: Dimensions.get('window').width * 0.5,
+          height: Dimensions.get('window').width * 0.5,
+          backgroundColor: '#f00',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+        underlayColor="#ccc"
+        onPress={() => handleButtonClick()}
+      >
+        <Text>Start</Text>
+      </TouchableHighlight>
+      {showForm && (
+        <View style={styles.form}>
+          <Animated.View
+            style={[
+              styles.reasonInputContainer,
+              { borderColor: borderColor },
+            ]}
+          >
+            <TextInput
+              style={styles.reasonInput}
+              onChangeText={setReason}
+              value={reason}
+              placeholder="Wprowadź powód"
+            />
+          </Animated.View>
+          <Button title="Zatwierdź" onPress={handleFormSubmit} />
+        </View>
+      )}
+      <View style={styles.quoteContainer}>
+        <Text style={styles.quoteText}>{quote}</Text>
+      </View>
+    </ScrollView>
   );
-  };
-  
-  const styles = StyleSheet.create({
+};
+
+const styles = StyleSheet.create({
   container: {
-  flexGrow: 1,
-  backgroundColor: '#121212',
-  alignItems: 'center',
-  justifyContent: 'center',
+    flexGrow: 1,
+    backgroundColor: '#121212',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
-  fontSize: 28,
-  fontWeight: 'bold',
-  color: '#FFFFFF',
-  marginBottom: 20,
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    marginBottom: 20,
   },
   timeText: {
-  fontSize: 20,
-  color: '#FFFFFF',
-  marginBottom: 20,
+    fontSize: 20,
+    color: '#FFFFFF',
+    marginBottom: 20,
   },
   quoteContainer: {
-  marginTop: 30,
-  paddingHorizontal: 20,
+    marginTop: 30,
+    paddingHorizontal: 20,
   },
   quoteText: {
-  fontSize: 18,
-  fontStyle: 'italic',
-  color: '#FFFFFF',
-  textAlign: 'center',
+    fontSize: 18,
+    fontStyle: 'italic',
+    color: '#FFFFFF',
+    textAlign: 'center',
   },
   form: {
-  marginTop: 20,
-  width: '80%',
+    marginTop: 20,
+    width: '80%',
+  },
+  reasonInputContainer: {
+    borderWidth: 1,
+    borderRadius: 4,
   },
   reasonInput: {
-  borderWidth: 1,
-  borderColor: '#FFFFFF',
-  borderRadius: 4,
-  color: '#FFFFFF',
-  padding: 10,
-  marginBottom: 10,
+    color: '#FFFFFF',
+    padding: 10,
+    marginBottom: 10,
   },
-  });
-  
-  export default SobrietyCounter;
+});
+
+export default SobrietyCounter;
